5c3412c52f104a0c06760947a324c8584f5267b9
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-04 23:37:38 +0100

    Handle basic database connection and CRUD operation

---
+++ app/server/database/database.py
@@ -0,0 +1,44 @@
+import motor.motor_asyncio
+from bson import ObjectId
+
+from app.server.database.database_helper import student_helper
+
+client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27018)
+
+database = client.students
+
+student_collection = database.get_collection('students_collection')
+
+
+async def retrieve_students():
+    students = []
+    async for student in student_collection.find():
+        students.append(student_helper(student))
+    return students
+
+
+async def add_student(student_data: dict) -> dict:
+    student = await student_collection.insert_one(student_data)
+    new_student = await student_collection.find_one({"_id": student.inserted_id})
+    return student_helper(new_student)
+
+
+async def retrieve_student(id: str) -> dict:
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        return student_helper(student)
+
+
+async def delete_student(id: str):
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        await student_collection.delete_one({"_id": ObjectId(id)})
+        return True
+
+
+async def update_student_data(id: str, student_record: dict):
+    # if student:
+    x = await student_collection.find_one_and_update({"_id": id}, {"$set": {student_record}})
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    print(x)
+    return student_helper(student)
