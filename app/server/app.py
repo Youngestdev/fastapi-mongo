427f2e9bd15c96cfdc04fb696fa8753511da4835
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-06 15:23:34 +0100

    Cleaned file.

--- app/server/app.py
+++ app/server/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+
 from .routes.student import router as StudentRouter
 
 app = FastAPI()
 
+
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app, sighs."}
+
 
 app.include_router(StudentRouter, tags=["Student"], prefix="/student")
