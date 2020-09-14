fb1660fba11394c129e0f0becef73fbf1661e074
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-14 01:28:21 +0100

    Add security to students route and included the admin routes

--- app/server/app.py
+++ app/server/app.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
+from .auth.jwt_bearer import JWTBearer
 from .routes.student import router as StudentRouter
+from .routes.admin import router as AdminRouter
 
 app = FastAPI()
 
+token_listener = JWTBearer()
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app, sighs."}
 
 
-app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
+app.include_router(StudentRouter, tags=["Students"], prefix="/student", dependencies=[Depends(token_listener)])
