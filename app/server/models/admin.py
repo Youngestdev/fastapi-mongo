2030eb4e844e30c823ab8cf33407921cd9414fe2
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-14 01:27:46 +0100

    Model for admin data

---
+++ app/server/models/admin.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class AdminModel(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdulazeez@x.com",
+                "password": "Yourpasswordgoes here."
+            }
+        }
