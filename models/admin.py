b7044e6d971fee66b564e582421cade81bf37144
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-04-26 14:56:51 +0100

    Rewrite application to use beanie as the ODM

--- models/admin.py
+++ models/admin.py
@@ -1,16 +1,44 @@
-from pydantic import BaseModel, Field, EmailStr
+from beanie import Document
+from fastapi.security import HTTPBasicCredentials
+from pydantic import BaseModel, EmailStr
 
 
-class AdminModel(BaseModel):
-    fullname: str = Field(...)
-    email: EmailStr = Field(...)
-    password: str = Field(...)
+class Admin(Document):
+    fullname: str
+    email: EmailStr
+    password: str
+
+    class Collection:
+        name = "admin"
 
     class Config:
         schema_extra = {
             "example": {
                 "fullname": "Abdulazeez Abdulazeez Adeshina",
-                "email": "abdulazeez@x.com",
-                "password": "Yourpasswordgoes here."
+                "email": "abdul@youngest.dev",
+                "password": "3xt3m#"
+            }
+        }
+
+
+class AdminSignIn(HTTPBasicCredentials):
+    class Config:
+        schema_extra = {
+            "example": {
+                "username": "abdul@youngest.dev",
+                "password": "3xt3m#"
+            }
+        }
+
+
+class AdminData(BaseModel):
+    fullname: str
+    email: EmailStr
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdul@youngest.dev",
             }
         }
